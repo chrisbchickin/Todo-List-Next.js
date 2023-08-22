25b7c22b5fd9452646e80fcab60fce1c3474653a
Chrisbchicken <chrsbnjmn@gmail.com>
2023-08-21 19:32:45 -0500

    full project

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    experimental: {
+        // Enable Server Actions
+        serverActions: true,
+      },
+}
+
+module.exports = nextConfig
