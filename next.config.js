a367a0b492586658176a3926e774861e2704d289
Chrisbchicken <chrsbnjmn@gmail.com>
2023-08-23 14:07:22 -0500

    Set enviroment variable

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const nextConfig = {
         // Enable Server Actions
         serverActions: true,
       },
+    env: {
+        DATABASE_URL: process.env.DATABASE_URL,
+      },
 }
 
 module.exports = nextConfig
